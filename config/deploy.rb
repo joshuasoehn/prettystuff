bc5547f5a0406bdc2f1aa8c400aa7c4d93cceeb7
Joshua <joshua.soehn@gmail.com>
2015-05-13 14:53:35 +0200

    new wip header, new layouts for pages, sticky footer

--- config/deploy.rb
+++ config/deploy.rb
@@ -59,17 +59,25 @@ task :deploy => :environment do
     # Put things to run locally before ssh
   end
   deploy do
-    # Put things that will set up an empty directory into a fully set-up
-    # instance of your project.
     invoke :'git:clone'
     invoke :'deploy:link_shared_paths'
     invoke :'deploy:cleanup'
+    queue 'cp -r /var/www/prettystuff/current/* /var/www/prettystuff/'
 
     to :launch do
       queue "mkdir -p #{deploy_to}/#{current_path}/tmp/"
       queue "touch #{deploy_to}/#{current_path}/tmp/restart.txt"
     end
   end
+end
+
+task :restart do
+  queue 'forever stop /var/www/prettystuff/index.js'
+  queue 'NODE_ENV=production forever start /var/www/prettystuff/index.js'
+end
+
+task :move do
+  queue 'cp -r /var/www/prettystuff/current/* /var/www/prettystuff/'
 end
 
 # For help in making your deploy script, see the Mina documentation:
